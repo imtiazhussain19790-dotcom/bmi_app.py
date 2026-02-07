e8cb1c2880af2ccf7a824730bfe8930d4fd1105e
imtiazhussain19790-dotcom <imtiazhussain19790@gmail.com>
2026-02-07 16:47:58 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,12 @@ height=st.number_input("Enter your height (in meters)")
 if height > 0:
     bmi=weight / (height * height)
     st.write(f"Your BMI is:{bmi}")
+    if bmi < 18.5:
+        st.warning ("(Underweight)")
+    elif 18.5 <= bmi < 25:
+        st.success("(Healty)")
+    elif 25 <= bmi < 30:
+        st.info("(Overweight)")
+    else:
+        st.error("(Obesity)")
+                                                    
